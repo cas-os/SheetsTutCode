a47511248d0975e123872124245ff4b5e884dd15
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-01 12:40:04 +0200

    /.1

--- SheetsTutCode/ContentView.swift
+++ SheetsTutCode/ContentView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isVisible = false
+    var depthLevel = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            Button {
+                self.isVisible.toggle()
+            } label: {
+                Text("1.Go to ContentView ! Depth level = \(depthLevel)")
+            }
             .padding()
+            .sheet(isPresented: $isVisible, content: {
+            ContentView(depthLevel: self.depthLevel + 1)
+                
+        })
+
+            Button {
+                self.isVisible.toggle()
+            } label: {
+                Text("2.Go to SheetView ! Depth level = \(depthLevel)")
+            }
+            padding()
+            .sheet(isPresented: $isVisible) {
+                SheetView()
+            }
+            
+
+        }
+        
+        
+
+
+
+    
     }
 }
+
+struct SheetView: View {
+    var body: some View {
+        Text("sheetView #2")
+            .font(.title)
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
