074f735e9af1a830328e2fd48084e05df2572a6f
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-01 11:19:28 +0200

    Initial Commit

---
+++ SheetsTutCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SheetsTutCode
+//
+//  Created by HAL-9001 on 01/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SheetsTutCode/SheetsTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetsTutCodeApp.swift
+//  SheetsTutCode
+//
+//  Created by HAL-9001 on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SheetsTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
